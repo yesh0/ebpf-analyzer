625511f7bd9e390f0b22114559494d90822c0f03
Yesh <yesh@aliyun.com>
2022-12-22 19:55:44 +0800

    fix: mark the compiler as non-no_std since it depends on cranelift

--- compiler/src/lib.rs
+++ compiler/src/lib.rs
@@ -1,6 +1,8 @@
 //! Compiles an eBPF program
 
-#![no_std]
+// FIXME: Cranelift-codegen is no longer no_std even if it keeps the #![no_std] attr there.
+//        We will need to write our own assembler, I'm afraid.
+// #![no_std]
 #![cfg_attr(feature = "nightly", feature(mixed_integer_ops))]
 #![forbid(missing_docs)]
 
